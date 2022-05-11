f09502feeff6587bdf5782f9871a8f8d0fd75b44
justina823 <laisumjustina.chan@my.jcu.edu.au>
2022-05-11 17:54:05 +1000

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -55,27 +55,7 @@ const Work = () => {
                 
               </div>
 
-              <div style={{backgroundImage:`url(${iheauthbeauty})`} }className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-                {/* hover effects*/}
-                <div className='opacity-0 group-hover:opacity-100'>
-                    <span className='text-2xl font-bold text-white tracking-wider'>
-                      Crypto
-
-                  </span>
-
-                  <div className='pt-8 text-center'>
-                  <a href='/Cryptocurrency'>
-                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                    </a>
-
-                  <a href='/'>
-                    <button className='text-center rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                    </a>
-                </div>
-                </div>
-                
-              </div>
+             
 
 
             
